27f717f2484895888f02ac08d1b065f112fa4d68
AmmarSaadDine <ammar.as.89@gmail.com>
2016-07-25 15:40:03 +0200

    changing paths

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,22 @@
 var cacheName = 'weatherPWA-step-5-1';  
 var filesToCache = [  
-  '/',  
-  '/index.html',  
-  '/scripts/app.js',  
-  '/styles/inline.css',  
-  '/images/clear.png',  
-  '/images/cloudy-scattered-showers.png',  
-  '/images/cloudy.png',  
-  '/images/fog.png',  
-  '/images/ic\_add\_white\_24px.svg',  
-  '/images/ic\_refresh\_white\_24px.svg',  
-  '/images/partly-cloudy.png',  
-  '/images/rain.png',  
-  '/images/scattered-showers.png',  
-  '/images/sleet.png',  
-  '/images/snow.png',  
-  '/images/thunderstorm.png',  
-  '/images/wind.png'  
+  '.',  
+  'index.html',  
+  'scripts/app.js',  
+  'styles/inline.css',  
+  'images/clear.png',  
+  'images/cloudy-scattered-showers.png',  
+  'images/cloudy.png',  
+  'images/fog.png',  
+  'images/ic\_add\_white\_24px.svg',  
+  'images/ic\_refresh\_white\_24px.svg',  
+  'images/partly-cloudy.png',  
+  'images/rain.png',  
+  'images/scattered-showers.png',  
+  'images/sleet.png',  
+  'images/snow.png',  
+  'images/thunderstorm.png',  
+  'images/wind.png'  
 ];
 self.addEventListener('install', function(e) {  
   console.log('[ServiceWorker] Install');  
